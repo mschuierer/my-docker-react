69226b5f610c8248436618b01e2a4025491d1345
mschuierer <schuierer.maxx@gmail.com>
2020-03-05 20:59:51 +0100

    changed app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was refreshed. Bla. Aa es geht! :) 
+          I was refreshed. Bla. Aa es geht! :) New feature now.
         </p>
         <a
           className="App-link"
